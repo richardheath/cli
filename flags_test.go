63cf6f0b700d307a655a4f7f6cdafb1db9dac57e
Richard Heath <rheath@humanlongevity.com>
2017-10-03 14:49:23 -0700

    test GetFlag

--- flags_test.go
+++ flags_test.go
@@ -37,3 +37,40 @@ func TestGetFlagValue(t *testing.T) {
 		}
 	}
 }
+
+func TestGetFlag(t *testing.T) {
+	flags := Flags{
+		Flag{
+			Key:       "option:test",
+			Name:      "test",
+			Shorthand: "t",
+		},
+	}
+	var tests = []struct {
+		input         string
+		expectedFlag  string
+		expectedFound bool
+	}{
+		{
+			input:         "option:test",
+			expectedFlag:  "test",
+			expectedFound: true,
+		},
+		{
+			input:         "option:unknown",
+			expectedFlag:  "",
+			expectedFound: false,
+		},
+	}
+
+	for _, test := range tests {
+		flag, found := flags.getFlag(test.input)
+		if !reflect.DeepEqual(flag.Name, test.expectedFlag) || !reflect.DeepEqual(found, test.expectedFound) {
+			t.Errorf("Input %q:\n", test.input)
+			t.Errorf("- Expected Value: %v", test.expectedFlag)
+			t.Errorf("- Actual Value: %v", flag)
+			t.Errorf("- Expected Found: %v", test.expectedFound)
+			t.Errorf("- Actual Found: %v", found)
+		}
+	}
+}
