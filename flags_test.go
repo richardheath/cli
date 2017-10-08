83811567b31e0ac8760178d41ddc774d43bbf404
Richard Heath <rheath@humanlongevity.com>
2017-10-07 22:08:57 -0700

    add tests

--- flags_test.go
+++ flags_test.go
@@ -38,10 +38,56 @@ func TestGetFlagValue(t *testing.T) {
 	}
 }
 
+func TestSetFlagValue(t *testing.T) {
+	flags := FlagValues{}
+	expectedValue := "richard"
+	flags.SetValue("option", "person", expectedValue)
+	value, _ := flags.GetValue("option", "person")
+	if !reflect.DeepEqual(value, expectedValue) {
+		t.Errorf("- Expected Value: %v", expectedValue)
+		t.Errorf("- Actual Value: %v", value)
+	}
+}
+
+func TestGetFlagsByType(t *testing.T) {
+	flags := FlagValues{
+		"option:user": "richard",
+		"option:log":  "console",
+		"setting:url": "www",
+	}
+
+	var tests = []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "option",
+			expected: []string{"user", "log"},
+		},
+		{
+			input:    "setting",
+			expected: []string{"url"},
+		},
+		{
+			input:    "unknown",
+			expected: []string{},
+		},
+	}
+
+	for _, test := range tests {
+		result := flags.GetFlagsByType(test.input)
+		if !reflect.DeepEqual(result, test.expected) {
+			t.Errorf("Input %q:\n", test.input)
+			t.Errorf("- Expected: %v", test.expected)
+			t.Errorf("- Actual: %v", result)
+		}
+	}
+}
 func TestGetFlag(t *testing.T) {
 	flags := Flags{
 		Flag{
 			Key:       "option:test",
+			FlagType:  "option",
 			Name:      "test",
 			Shorthand: "t",
 		},
@@ -64,7 +110,8 @@ func TestGetFlag(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		flag, found := flags.getFlag(test.input)
+		flagType, flagName := splitFlagKey(test.input)
+		flag, found := flags.getFlag(flagType, flagName)
 		if !reflect.DeepEqual(flag.Name, test.expectedFlag) || !reflect.DeepEqual(found, test.expectedFound) {
 			t.Errorf("Input %q:\n", test.input)
 			t.Errorf("- Expected Value: %v", test.expectedFlag)
